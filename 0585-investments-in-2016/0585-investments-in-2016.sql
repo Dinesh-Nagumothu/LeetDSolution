6827f11dd3d0e5ad21968f94c538bb2a44e90724
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-04-15 16:54:23 +0530

    Time: 545 ms (79.78%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,15 +1,13 @@
-/* Write your T-SQL query statement below */
-WITH unique_location AS(
-    SELECT lat, lon FROM Insurance GROUP BY lat, lon HAVING COUNT(pid) = 1
-),
-all_valid_tiv2015 AS(
-    SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015 HAVING COUNT(pid) > 1
-)
+# Write your MySQL query statement below
 
-SELECT ROUND(SUM(tiv_2016 * 1.0), 2) AS tiv_2016
-FROM Insurance i
-WHERE EXISTS (
-    SELECT 1 FROM unique_location ul 
-    WHERE i.lat = ul.lat AND i.lon = ul.lon
-)
-AND tiv_2015 IN (SELECT tiv_2015 FROM all_valid_tiv2015);
+
+SELECT
+ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM(SELECT
+*,
+COUNT(*) OVER(PARTITION BY lat, lon ORDER BY lat, lon) AS cnt,
+COUNT(*) OVER(PARTITION BY tiv_2015) AS cnt2
+
+FROM Insurance
+) t
+WHERE cnt=1 AND cnt2>1
