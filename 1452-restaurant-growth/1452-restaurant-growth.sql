57d638b07591f1b364dffdc13a99095df68fc883
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-04-15 16:46:55 +0530

    Time: 383 ms (72.11%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -1,26 +1,16 @@
-WITH DailyTotal AS (
-    SELECT 
-        visited_on,
-        SUM(amount) AS amount
+# Write your MySQL query statement below
+WITH daily_amount AS (
+    SELECT visited_on, SUM(amount) AS amount
     FROM Customer
     GROUP BY visited_on
 ),
-Rolling7Days AS (
-    SELECT 
-        dt1.visited_on,
-        SUM(dt2.amount) AS total_amount,
-        ROUND(SUM(dt2.amount) / 7, 2) AS average_amount
-    FROM DailyTotal dt1
-    JOIN DailyTotal dt2 
-        ON dt2.visited_on BETWEEN DATE_SUB(dt1.visited_on, INTERVAL 6 DAY) AND dt1.visited_on
-    GROUP BY dt1.visited_on
+sum_amt AS (
+    SELECT visited_on, 
+        SUM(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+        ROUND(AVG(amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS average_amount
+    FROM daily_amount
 )
-SELECT 
-    visited_on, 
-    total_amount AS amount, 
-    average_amount
-FROM Rolling7Days
-WHERE visited_on >= (
-    SELECT MIN(visited_on) FROM DailyTotal
-) + INTERVAL 6 DAY
+SELECT visited_on, amount, average_amount
+FROM sum_amt
+WHERE visited_on >= DATE_ADD((SELECT MIN(visited_on) FROM daily_amount), INTERVAL 6 DAY)
 ORDER BY visited_on;
