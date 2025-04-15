8cd2b650c84e73b555f1c71bade43a69fa9abd14
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-04-15 16:42:17 +0530

    Time: 1225 ms (85.24%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,27 +1,14 @@
 # Write your MySQL query statement below
-WITH 
-TheMostActiveUser AS (
-    SELECT name
-    FROM 
-        Users
-        NATURAL JOIN MovieRating
-    GROUP BY user_id
-    ORDER BY COUNT(*) DESC, name
-    LIMIT 1
-),
-TheBestMovieFebruary AS (
-    SELECT title
-    FROM
-        Movies
-        NATURAL JOIN MovieRating
-    WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29'
-    GROUP BY movie_id
-    ORDER BY AVG(rating) DESC, title
-    LIMIT 1
-)
-
-SELECT name AS results
-FROM TheMostActiveUser
+# Write your MySQL query statement below
+(SELECT name as results
+FROM Users U JOIN MovieRating R ON U.user_id = R.user_id
+GROUP BY U.user_id
+ORDER BY Count(*) DESC, name
+LIMIT 1)
 UNION ALL
-SELECT title
-FROM TheBestMovieFebruary
+(SELECT m.title as results
+FROM Movies m Right JOIN MovieRating R ON m.movie_id = R.movie_id
+WHERE R.created_at BETWEEN '2020-02-01' AND '2020-02-28'
+GROUP BY m.movie_id
+ORDER BY avg(rating) DESC, m.title
+LIMIT 1)
