58df2134fc247e83a40fe6b0f3006b7e3f3517cb
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-04-15 13:09:23 +0530

    Time: 739 ms (75.47%), Space: 49.4 MB (27.36%) - LeetHub

--- 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.py
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.py
@@ -1,45 +1,24 @@
+from sortedcontainers import SortedList
 from typing import List
-
-class FenwickTree:
-    def __init__(self, size):
-        self.tree = [0] * (size + 2)  # 1-based indexing
-
-    def update(self, i, delta):
-        i += 1  # convert to 1-based
-        while i < len(self.tree):
-            self.tree[i] += delta
-            i += i & -i
-
-    def query(self, i):
-        i += 1
-        res = 0
-        while i > 0:
-            res += self.tree[i]
-            i -= i & -i
-        return res
 
 class Solution:
     def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
-        n = len(nums1)
-        pos2 = {num: i for i, num in enumerate(nums2)}
-        mapped = [pos2[num] for num in nums1]
 
-        left_tree = FenwickTree(n)
-        left_counts = [0] * n
+        index_map = {num: i for i, num in enumerate(nums2)}
+  
+        indices = [index_map[num] for num in nums1]
 
-        for i in range(n):
-            left_counts[i] = left_tree.query(mapped[i] - 1)
-            left_tree.update(mapped[i], 1)
+        left_counts = []
+        left_sorted = SortedList()
+        for idx in indices:
+            left_counts.append(left_sorted.bisect_left(idx))
+            left_sorted.add(idx)
 
-        right_tree = FenwickTree(n)
-        right_counts = [0] * n
-
-        for i in range(n - 1, -1, -1):
-            right_counts[i] = right_tree.query(n - 1) - right_tree.query(mapped[i])
-            right_tree.update(mapped[i], 1)
-
-        result = 0
-        for i in range(n):
-            result += left_counts[i] * right_counts[i]
-
-        return result
+        right_counts = []
+        right_sorted = SortedList()
+        for idx in reversed(indices):
+            right_counts.append(len(right_sorted) - right_sorted.bisect_right(idx))
+            right_sorted.add(idx)
+        right_counts.reverse() 
+        
+        return sum(left * right for left, right in zip(left_counts, right_counts))
