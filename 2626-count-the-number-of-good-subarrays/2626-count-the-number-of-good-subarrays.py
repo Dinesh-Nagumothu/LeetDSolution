4b306a1f1a173963e9f604aa3ea2fa27c05f119c
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-04-16 12:58:26 +0530

    Time: 97 ms (98.38%), Space: 34.4 MB (45.31%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
@@ -1,27 +1,19 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
-        # Initialize the frequency map, number of pairs, and result
-        freq = defaultdict(int)
-        pairs = 0
-        result = 0
-        left = 0
         
-        # Iterate over the array with a sliding window approach
-        for right in range(len(nums)):
-            # Add the current number to the frequency map
-            num = nums[right]
-            # If num was seen before, we can form pairs with the previous occurrences of num
-            pairs += freq[num]
-            # Increment the frequency of the current number
-            freq[num] += 1
-            
-            # While the number of pairs is >= k, shrink the window from the left
-            while pairs >= k:
-                # If we found a valid subarray, add the number of subarrays ending at `right`
-                result += len(nums) - right
-                # Reduce the pair count by removing the leftmost element
-                freq[nums[left]] -= 1
-                pairs -= freq[nums[left]]
-                left += 1
-        
-        return result
+
+        # find the smallest l such that nums[l:r+1] has pairs <k
+        # then ans += l
+        n, l, ans, dic, cnt = len(nums), 0, 0, {}, 0
+
+        for a in nums:
+            cnt += dic.get(a, 0)
+            dic[a] = dic.get(a, 0)+1
+
+            while cnt>=k:
+                dic[nums[l]]-=1
+                cnt -= dic[nums[l]]
+                l+=1
+
+            ans += l
+        return ans
