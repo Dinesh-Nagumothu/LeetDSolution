0f829b063394ebecf7e1113cb51bf1724141a931
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-04-18 08:06:15 +0530

    Time: 2 ms (97.94%), Space: 17.7 MB (96.89%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,22 +1,22 @@
 class Solution:
+    h = ["1"]  
     def countAndSay(self, n: int) -> str:
-        if n == 1:
-            return "1"
-        
-        def run_length_encode(s: str) -> str:
-            result = []
-            i = 0
-            while i < len(s):
+        def u(n):
+            if n <= len(Solution.h):  
+                return Solution.h[n - 1]  
+            else:
+                prev = u(n - 1)  
+                result = ""
                 count = 1
-                while i + 1 < len(s) and s[i] == s[i + 1]:
-                    i += 1
-                    count += 1
-                result.append(str(count) + s[i])
-                i += 1
-            return ''.join(result)
-        
-        result = "1"
-        for _ in range(n - 1):
-            result = run_length_encode(result)
-        
-        return result
+
+                for i in range(1, len(prev)):
+                    if prev[i] == prev[i - 1]:  
+                        count += 1
+                    else:
+                        result += f"{count}{prev[i - 1]}"  
+                        count = 1  
+
+                result += f"{count}{prev[-1]}"
+                Solution.h.append(result)  
+                return result
+        return u(n)
