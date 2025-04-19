f1cd7c70b0710720f5576b7b071229524b4e5c18
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-04-19 12:20:26 +0530

    Time: 239 ms (46.46%), Space: 31.6 MB (53.88%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -1,19 +1,14 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        def binSearch(l,r,t):
-            while l<=r:
-                mid=(l+r)//2
-                if nums[mid]>=t:
-                    r=mid-1
-                else:
-                    l=mid+1
-            return r
-
         nums.sort()
-        res=0
-        for i in range(len(nums)):
-            lo=lower-nums[i]
-            up=upper-nums[i]
-            res+=(binSearch(i+1,len(nums)-1,up+1)-binSearch(i+1,len(nums)-1,lo))
-        return res
+        n = len(nums)
+        count = 0
         
+        for i in range(n):
+            # We want to find the number of j > i such that:
+            # lower - nums[i] <= nums[j] <= upper - nums[i]
+            left = bisect_left(nums, lower - nums[i], i + 1, n)
+            right = bisect_right(nums, upper - nums[i], i + 1, n)
+            count += (right - left)
+        
+        return count
