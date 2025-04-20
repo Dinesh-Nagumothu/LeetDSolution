964a29a485905467c64505fe96e93a2fd167101e
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-04-20 15:11:15 +0530

    Time: 0 ms (100%), Space: 17.9 MB (76.07%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.py
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -1,11 +1,16 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        counter = Counter(answers)
-        total = 0
+        count_map = {}
+        for a in answers:
+            if a in count_map:
+                count_map[a] += 1
+            else:
+                count_map[a] = 1
         
-        for answer, count in counter.items():
-            group_size = answer + 1
-            groups = ceil(count / group_size)
-            total += groups * group_size
+        total = 0
+        for a in count_map:
+            group_size = a + 1
+            num_rabbits = ((count_map[a] + group_size - 1) // group_size) * group_size
+            total += num_rabbits
         
         return total
