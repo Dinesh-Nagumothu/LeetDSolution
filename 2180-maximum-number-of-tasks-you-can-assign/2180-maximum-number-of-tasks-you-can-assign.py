29f64a5e8231b74aea0bbca815e4d18d3d2d7107
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-05-01 16:46:10 +0530

    Time: 291 ms (99.69%), Space: 26.1 MB (70.22%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.py
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.py
@@ -1,38 +1,37 @@
-from sortedcontainers import SortedList
-
 class Solution:
-    def maxTaskAssign(self, tasks, workers, pills, strength):
+    def maxTaskAssign(self, tasks: List[int], workers: List[int], pills: int, strength: int) -> int:
         tasks.sort()
-        workers.sort()
+        workers.sort(reverse=True)
 
-        def can_assign(k):
-            task_ptr = k - 1
-            available_workers = SortedList(workers[-k:])
-            pills_left = pills
-
-            for i in range(k - 1, -1, -1):
-                task = tasks[i]
-                if available_workers and available_workers[-1] >= task:
-                    available_workers.pop(-1)
-                elif pills_left > 0:
-                    idx = available_workers.bisect_left(task - strength)
-                    if idx == len(available_workers):
-                        return False
-                    available_workers.pop(idx)
-                    pills_left -= 1
-                else:
-                    return False
+        def can_solve(k: int) -> bool:
+            w = 0
+            p = pills
+            q = deque[int]()
+            for t in range(k - 1, -1, -1):
+                if len(q) == 0 and workers[w] >= tasks[t]:
+                    w += 1
+                    continue
+                if len(q) > 0 and q[0] >= tasks[t]:
+                    q.popleft()
+                    continue
+                while w < k and workers[w] + strength >= tasks[t]:
+                    q.append(workers[w])
+                    w += 1
+                if len(q) > 0 and p > 0:
+                    q.pop()
+                    p -= 1
+                    continue
+                return False
             return True
 
         left, right = 0, min(len(tasks), len(workers))
-        result = 0
-
+        closest = 0
         while left <= right:
             mid = (left + right) // 2
-            if can_assign(mid):
-                result = mid
+            if can_solve(mid):
+                closest = mid
                 left = mid + 1
             else:
                 right = mid - 1
 
-        return result
+        return closest
