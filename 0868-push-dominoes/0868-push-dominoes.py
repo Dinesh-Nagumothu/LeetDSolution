78ecab6193e76ed9fbf17779f3ccebbac0a26c35
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-05-02 13:11:44 +0530

    Time: 87 ms (91.78%), Space: 18.9 MB (96.38%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.py
+++ 0868-push-dominoes/0868-push-dominoes.py
@@ -1,38 +1,11 @@
-class Solution:
+class Solution:         
     def pushDominoes(self, dominoes: str) -> str:
-        n = len(dominoes)
-        forces = [0] * n
+        temp = ''
         
-        # Left to Right: Apply positive force for 'R'
-        force = 0
-        for i in range(n):
-            if dominoes[i] == 'R':
-                force = n
-            elif dominoes[i] == 'L':
-                force = 0
-            else:
-                force = max(force - 1, 0)
-            forces[i] += force
-        
-        # Right to Left: Apply negative force for 'L'
-        force = 0
-        for i in range(n - 1, -1, -1):
-            if dominoes[i] == 'L':
-                force = n
-            elif dominoes[i] == 'R':
-                force = 0
-            else:
-                force = max(force - 1, 0)
-            forces[i] -= force
-        
-        # Determine final state
-        result = []
-        for f in forces:
-            if f > 0:
-                result.append('R')
-            elif f < 0:
-                result.append('L')
-            else:
-                result.append('.')
-        
-        return ''.join(result)
+        while dominoes != temp:
+            temp = dominoes
+            dominoes = dominoes.replace('R.L', 'xxx')       
+            dominoes = dominoes.replace('R.', 'RR')        
+            dominoes = dominoes.replace('.L', 'LL')         
+
+        return  dominoes.replace('xxx', 'R.L')              
