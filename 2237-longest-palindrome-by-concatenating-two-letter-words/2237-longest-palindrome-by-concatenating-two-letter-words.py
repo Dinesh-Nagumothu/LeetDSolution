00640450c2658588d0af5fbc1baa6fa8772e8576
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-05-25 11:28:28 +0530

    Time: 39 ms (95.85%), Space: 35.8 MB (12.27%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,28 +1,15 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
-        count = Counter(words)
-        length = 0
-        center_used = False
-
-        for word in list(count.keys()):
-            rev = word[::-1]
-            if word == rev:
-                # Use as many pairs as possible (e.g., "aa", "bb")
-                pairs = count[word] // 2
-                length += pairs * 4
-                count[word] -= pairs * 2
-            elif rev in count:
-                # Use min of both word and its reverse
-                pairs = min(count[word], count[rev])
-                length += pairs * 4
-                count[word] -= pairs
-                count[rev] -= pairs
-
-        # Try placing one remaining symmetric word like "aa" in the center
-        if not center_used:
-            for word in count:
-                if word[0] == word[1] and count[word] > 0:
-                    length += 2
-                    break
-
-        return length
+        from collections import Counter
+        cnt=Counter(words)
+        res=0
+        center=False
+        for w in list(cnt.keys()):
+        	rev=w[::-1]
+        	if w==rev:
+        		res+=(cnt[w]//2)*2
+        		if cnt[w]%2:
+        			center=True
+        	elif w<rev:
+        		res+=2*min(cnt[w],cnt[rev])
+        return (res+(1 if center else 0))*2
