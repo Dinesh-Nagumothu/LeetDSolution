c79cb136e7ea7d72c3e73db8b4683d7b14da8d77
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-05-26 10:23:02 +0530

    Time: 1306 ms (98.88%), Space: 76.1 MB (98.2%) - LeetHub

--- 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.py
+++ 1986-largest-color-value-in-a-directed-graph/1986-largest-color-value-in-a-directed-graph.py
@@ -1,37 +1,44 @@
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
-        n = len(colors)
-        graph = defaultdict(list)
-        indegree = [0] * n
-        
-        # Build graph and indegree count
-        for u, v in edges:
-            graph[u].append(v)
-            indegree[v] += 1
-        
-        # DP array: dp[i][c] -> max number of color c (0..25) along path to node i
-        dp = [[0] * 26 for _ in range(n)]
-        
         queue = deque()
+        dic = {}
+        n = len(colors)
+        indegree = [0] * n
+        count = [{} for _ in range(n)]
+        max_count = 0
+
+        for x, y in edges:
+            if y in dic:
+                dic[y].append(x)
+            else:
+                dic[y] = [x]
+            indegree[x] += 1
+        
         for i in range(n):
             if indegree[i] == 0:
                 queue.append(i)
-                dp[i][ord(colors[i]) - ord('a')] = 1
-        
-        visited = 0
-        max_color_value = 0
-        
+
         while queue:
-            node = queue.popleft()
-            visited += 1
-            for neighbor in graph[node]:
-                for c in range(26):
-                    # Update neighbor's color count
-                    count = dp[node][c] + (1 if c == ord(colors[neighbor]) - ord('a') else 0)
-                    dp[neighbor][c] = max(dp[neighbor][c], count)
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
-            max_color_value = max(max_color_value, max(dp[node]))
+            cur = queue.popleft()
+            
+            color = colors[cur]
+            if color in count[cur]:
+                count[cur][color] += 1
+            else:
+                count[cur][color] = 1
+            max_count = max(max_count, count[cur][color])
+            if cur in dic:
+                for neighbor in dic[cur]:
+                    for key, val in count[cur].items():
+                        if key in count[neighbor]:
+                            count[neighbor][key] = max(val, count[neighbor][key])
+                        else:
+                            count[neighbor][key] = val
+                    indegree[neighbor] -= 1
+                    if indegree[neighbor] == 0:
+                        queue.append(neighbor)
         
-        return max_color_value if visited == n else -1
+        for i in indegree:
+            if i != 0:
+                return -1
+        return max_count
