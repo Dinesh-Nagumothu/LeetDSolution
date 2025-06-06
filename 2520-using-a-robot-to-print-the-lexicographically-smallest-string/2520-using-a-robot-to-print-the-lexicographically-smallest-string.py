03085b3e9d23f24e43949f17ef1cb38ac2c32ac4
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-06-06 09:27:57 +0530

    Time: 114 ms (99.62%), Space: 21 MB (43.51%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,20 +1,22 @@
 class Solution:
-    def robotWithString(self, s: str) -> str:
-        n = len(s)
-        result = []
-        t = []
-        
-        # Step 1: Create min_suffix array
-        min_suffix = [None] * n
-        min_suffix[-1] = s[-1]
-        for i in range(n - 2, -1, -1):
-            min_suffix[i] = min(s[i], min_suffix[i + 1])
-        
-        # Step 2: Greedy simulation
-        for i in range(n):
-            t.append(s[i])
-            # While we can pop from t to write to result
-            while t and (i == n - 1 or t[-1] <= min_suffix[i + 1]):
-                result.append(t.pop())
-        
-        return ''.join(result)
+
+	def robotWithString(self, s: str) -> str:
+		min_char = min(s)
+		min_char_count = s.count(min_char)
+
+		res, stack, last_idx = "", [], len(s) - 1
+		for idx, char in enumerate(s):
+			if char == min_char:
+				res += char
+				min_char_count -= 1
+				if min_char_count == 0:
+					if idx >= last_idx:
+						break
+					min_char = min(s[idx + 1:])
+					min_char_count = s[idx + 1:].count(min_char)
+					while stack and stack[-1] <= min_char:
+						res += stack.pop()
+			else:
+				stack.append(char)
+
+		return res + "".join(stack[::-1])
