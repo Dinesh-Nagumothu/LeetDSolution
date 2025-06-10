fa4ea35152c1a15e45fea546e2fd26aeee124585
Dinesh <136153839+Dinesh-Nagumothu@users.noreply.github.com>
2025-06-10 11:18:38 +0530

    Time: 2 ms (69.59%), Space: 17.9 MB (47.42%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.py
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.py
@@ -1,16 +1,12 @@
-from collections import Counter
-
 class Solution:
     def maxDifference(self, s: str) -> int:
-        freq = Counter(s)
-        
-        odd_freqs = [count for count in freq.values() if count % 2 == 1]
-        even_freqs = [count for count in freq.values() if count % 2 == 0]
-        
-        # If we have no odd or no even frequencies, no valid pair exists
-        if not odd_freqs or not even_freqs:
-            return 0
-        
-        # Find the max difference
-        max_diff = max(a1 - a2 for a1 in odd_freqs for a2 in even_freqs)
-        return max_diff
+        record = Counter(s)
+
+        max_odd = 0
+        min_even = float('inf')
+        for ch in record:
+            if record[ch] % 2 != 0:
+                max_odd = max(max_odd, record[ch])
+            if record[ch] % 2 == 0:
+                min_even = min(min_even, record[ch])
+        return max_odd - min_even
